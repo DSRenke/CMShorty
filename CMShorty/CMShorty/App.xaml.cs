543029389787893a6c1e797a5159b4c3714926f4
DSR <renke@element44.de>
2023-06-28 18:57:57 +0200

    ## Init Commit - Xamarin.Forms Projekt erstellt - Bundle-ID & Package gesetzt

---
+++ CMShorty/CMShorty/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace CMShorty
+{
+    using Xamarin.Forms;
+
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+
+            this.MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
